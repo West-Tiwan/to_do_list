9ba9d3be3e427e5fed906cf230faaa5e434ada04
West-Tiwan <xyzmohit.singh@gmail.com>
2024-02-19 19:10:49 +0530

    show "no task added" if no task is there

--- app/page.js
+++ app/page.js
@@ -11,13 +11,18 @@ const Page = () => {
         setDesc('');
         setTitle('');
     }
-    let renderTask = <h2>No task added</h2>
+
+    let renderTask = <h2 className="text-3xl font-bold text-center">No task added</h2>
+    if (mainTask.length>0){
     renderTask = mainTask.map((t,i)=>{
-        return <div className="flex justify-around">
-            <h5>{t.title}</h5>
-            <h6>{t.desc}</h6>
-        </div>
-    })
+        return <li><div className="flex justify-between">
+            <h5 className="text-2xl font-semibold">{t.title}</h5>
+            <h6 className="text-xl font-semibold">{t.desc}</h6>
+        </div></li>
+    })}
+     else {
+        renderTask = <h2 className="text-3xl font-bold text-center">No task added</h2>
+    }
     return (
         <>
             <h1 className="text-white bg-black flex justify-center items-center py-4 text-5xl font-bold">West Tiwan's To-Do List</h1>
@@ -32,9 +37,9 @@ const Page = () => {
             </form>
             <hr/>
             <div className="p-8 bg-purple-100">
-                <div className="flex justify-around">
-                    <h3>Title</h3>
-                    <h3>Description</h3>
+                <div className="flex justify-between">
+                    <h3 className="font-bold text-3xl">Title</h3>
+                    <h3 className="font-bold text-3xl">Description</h3>
                 </div>
                 <ul>
                     {renderTask}
